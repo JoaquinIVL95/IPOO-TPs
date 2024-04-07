bdbdf07dd7213521b224c6b4eb1940241a177e0b
JoaquinIVL95 <joaquinIVl95@gmail.com>
2024-04-07 17:44:08 -0300

    Finalizacion de TPE y actualizacion de carpetas

--- TPE/Viaje.php
+++ TPE/Viaje.php
@@ -7,13 +7,15 @@ Class Viaje{
     private $destino;
     private $maxCantP;
     private $pasajeros;
+    private $responsable;
 
-    public function __construct($codigoViaje , $destino, $capacidadMax , $aPasajeros)
+    public function __construct($codigoViaje , $destino, $capacidadMax )
     {
         $this->viajeCod = $codigoViaje; 
         $this->destino=$destino;
         $this->maxCantP= $capacidadMax;
-        $this->pasajeros =$aPasajeros;  
+        $this->responsable = array();  
+        $this->pasajeros = array();
     }
 
     public function getViajeCod(){
@@ -44,6 +46,71 @@ Class Viaje{
         $this->pasajeros=$aPasajeros;
     }
 
+    public function getResponsable(){
+        return $this->responsable;
+    }
+    public function setResponsable($responsable){
+        $this->responsable=$responsable;
+    }
+
+    public function agregarResponsable(ResponsableV $responsableV){
+        $error = false;
+        $responsable = $this->getResponsable();
+        foreach ($responsable as $p) {
+            if ($p->getDniResp() == $responsableV->getDniResp()) {
+                $error = true;
+                
+            }
+        }
+        if (!$error) {
+            $this->responsable[] = $responsableV;
+        }
+        return $error;
+    }
+
+
+    public function agregarPasajeros(Personas $pasajero){
+        $error = false;
+        $pasajeros = $this->getPasajeros();
+        foreach ($pasajeros as $p) {
+            if ($p->getDni() == $pasajero->getDni()) {
+                $error = true;
+                
+            }
+        }
+        if (!$error) {
+            $this->pasajeros[] = $pasajero;
+        }
+        return $error;
+    }
+
+
+
+    public function eliminarPasajero($dni) {
+        $pasajeros = $this->getPasajeros();
+        $eliminado = false;
+        foreach ($pasajeros as $index => $pasajero) {
+            if ($pasajero->getDni() == $dni) {
+                unset($this->pasajeros[$index]);
+                return true; // Elemento encontrado y eliminado
+            }
+        }
+        return false; // Elemento no encontrado
+    }
+
+
+
+
+    public function listaPasajeros(){
+        $lista = $this->getPasajeros();
+        $cadena = $this->devolverArreglos($lista);
+
+        return $cadena;
+    }
+
+
+
+
     private function devolverArreglos($arreglo){
         $cadena= "\n";
         foreach ($arreglo as $elemento){
@@ -56,7 +123,7 @@ Class Viaje{
 
     public function __toString()
     {
-        $cadenaPasajeros= $this->devolverArreglos($this->getPasajeros());
-        return $this->getViajeCod()." ".$this->getDestino() . " ". $this->getMaxCantP() . " ". $cadenaPasajeros;
+        // $cadenaPasajeros= $this->devolverArreglos($this->getPasajeros());
+        return "Codigo del viaje N° " .  $this->getViajeCod(). "\n". " Con destino a ".$this->getDestino(). "\n" . " La capacidad maxima de pasajeros es de ". $this->getMaxCantP() . " personas \n";
     }
 }
